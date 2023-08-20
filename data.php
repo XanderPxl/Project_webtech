1e7e454fa43b64c6cf54e3841851941249682226
Xander <xander.aerts@outlook.com>
2023-08-20 22:25:48 +0200

    The live page has a link to the About_page that has further been written added and the caddy file has an updated version now as well as the data.php that has more debug info to print out.

--- data.php
+++ data.php
@@ -2,10 +2,35 @@
 $body = file_get_contents("php://input");
 $data = json_decode($body, true);
 
-if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
-    echo "JSON decoding error: " . json_last_error_msg();
-} else {
-    echo "Decoded data: " . print_r($data, true);
+if (!$data)
+{
+        die("Error decoding JSON: " . json_last_error_msg());
 }
 
+echo "JSON Payload: " .print_r($data, true) . "<br>";
+
+$dbconn = pg_connect("host=localhost dbname=dbxander user=xander password=Xander8963");
+
+
+if (!$dbconn)
+{
+        die("Connection failed: " . pg_last_error());
+}
+
+$temperature = $data['temperature'];
+
+echo "Temperature: " . $temperature ."<br>";
+
+$query = "INSERT INTO temperature_data (temperature) VALUES ($temperature)";
+$result = pg_query($dbconn, $query);
+
+if (!$result)
+{
+        die("Error in SQL query: " . pg_last_error());
+}
+
+pg_close($dbconn);
+
+
+echo "Data inserted successfully!";
 ?>
